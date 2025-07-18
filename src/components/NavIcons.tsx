f108a3f380ca9a1a046522bab497a47ffbe56cad
fal hamza <fal.hamza98@gmail.com>
2025-07-18 02:54:39 +0200

    searchBar Done

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,7 +1,67 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+// import { useRouter } from "next/router";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 const NavIcons = () => {
-  return <div>NavIcons</div>;
+  //PROFILE
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  //CART
+  const [iscCartOpen, setIscCartOpen] = useState(false);
+
+  // FUNCTION PROFLE
+
+  const router = useRouter();
+
+  //TEMPORARY
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+
+    setIsProfileOpen((prev) => !prev);
+  };
+  return (
+    <div className=' flex items-center justify-center gap-4 xl:gap-6'>
+      <Image
+        src='/profile.png'
+        alt='profile'
+        width={22}
+        height={22}
+        className=' cursor-pointer'
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className=''>
+          {/* 1 */}
+          <Link href='/' className=''>
+            Profile
+          </Link>
+          {/* 2 */}
+          <div className='mt-2 cursor-pointer'>Logout</div>
+        </div>
+      )}
+      <Image
+        src='/notification.png'
+        alt='profile'
+        width={22}
+        height={22}
+        className=' cursor-pointer'
+      />
+      <Image
+        src='/cart.png'
+        alt='profile'
+        width={22}
+        height={22}
+        className=' cursor-pointer'
+      />
+    </div>
+  );
 };
 
 export default NavIcons;
