dd4d56f939a5584656734079c973750f11a23aab
fal hamza <fal.hamza98@gmail.com>
2025-07-30 02:03:58 +0200

    pageList and footer

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,36 @@
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
 
+// color: rgb(65, 105, 255);
+// color: rgb(102, 136, 240);
 
 const page = () => {
-  const ham = "ybxubxu";
   return (
-    <div>pageList</div>
-    
-  )
-}
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+      {/* COMPAIGN */}
+      <div className=' bg-pink-50 px-4 flex justify-between h-64'>
+        <div className=' md:w-2/3 w-full flex flex-col items-center justify-center gap-8 '>
+          <h1 className=' text-xl md:text-3xl font-semibold leading-[48px] text-gray-700'>
+            Grap up to 50% on
+            <br />
+            Selected Product
+          </h1>
+          <button className=' rounded-3xl bg-lama text-white px-5 py-3 text-sm'>
+            Buy Now
+          </button>
+        </div>
+        <div className=' hidden md:block relative w-1/3'>
+          <Image src='/woman.png' alt='' fill className=' object-contain' />
+        </div>
+      </div>
+      {/* Filter */}
+      <Filter />
+      {/* ListProduct */}
+      <h1 className=' mt-12 text-xl font-semibold'>Shoes For You!</h1>
+      <ProductList />
+    </div>
+  );
+};
 
-export default page
+export default page;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,137 @@
+import Image from "next/image";
+import Link from "next/link";
 
+// icons
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24'>
+      {/* TOP */}
+      <div className='flex flex-col md:flex-row justify-between gap-24'>
+        {/* LEFT */}
+        <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
+          <Link href='/' className=''>
+            <div className='text-2xl tracking-wide'>Hamza</div>
+          </Link>
+          <p>
+            Morocco, Marakech Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Quas, voluptatum.
+          </p>
+          <span className=' font-semibold'>fal.hamza98@gmail.com</span>
+          <span className=' font-semibold'>0666541236</span>
+          <ul className='flex gap-6'>
+            <Link
+              href='/'
+              className=' w-[50px] h-[50px]  transition-transform duration-500 hover:scale-150 ease-out flex items-center justify-center'
+            >
+              <FaFacebookF size={20} color='#3b5998' />
+            </Link>
+            <Link
+              href='/'
+              className=' w-[50px] h-[50px]  transition-transform duration-500 hover:scale-150 ease-out flex items-center justify-center'
+            >
+              <FaInstagram size={20} color='#E1306C' />
+            </Link>
+            <Link
+              href='/'
+              className=' w-[50px] h-[50px]  transition-transform duration-500 hover:scale-150 ease-out flex items-center justify-center'
+            >
+              <FaTwitter size={20} />
+            </Link>
+            <Link
+              href='/'
+              className=' w-[50px] h-[50px]  transition-transform duration-500 hover:scale-150 ease-out flex items-center justify-center'
+            >
+              <FaWhatsapp size={20} color='#25D366' />
+            </Link>
+          </ul>
+        </div>
+        {/* CENTER */}
+        <div className=' hidden lg:flex justify-between w-1/2 '>
+          {/* [01] */}
+          <div className='flex flex-col justify-between'>
+            <h1 className=' font-medium text-lg'>COMPANY</h1>
 
-export default Footer
+            <div className='flex flex-col gap-6'>
+              <Link href='/'>About Us</Link>
+              <Link href='/'>Careers</Link>
+              <Link href='/'>Affiliates</Link>
+              <Link href='/'>Blog</Link>
+              <Link href='/'>Contact Us</Link>
+            </div>
+          </div>
+          {/* [01] */}
+          <div className='flex flex-col justify-between'>
+            <h1 className=' font-medium text-lg'>COMPANY</h1>
+
+            <div className='flex flex-col gap-6'>
+              <Link href='/'>About Us</Link>
+              <Link href='/'>Careers</Link>
+              <Link href='/'>Affiliates</Link>
+              <Link href='/'>Blog</Link>
+              <Link href='/'>Contact Us</Link>
+            </div>
+          </div>
+          {/* [01] */}
+          <div className='flex flex-col justify-between'>
+            <h1 className=' font-medium text-lg'>COMPANY</h1>
+
+            <div className='flex flex-col gap-6'>
+              <Link href='/'>About Us</Link>
+              <Link href='/'>Careers</Link>
+              <Link href='/'>Affiliates</Link>
+              <Link href='/'>Blog</Link>
+              <Link href='/'>Contact Us</Link>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className=' w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
+          <h1 className=' font-medium text-lg'>SUBSCRIBE</h1>
+          <p className=''>
+            Be the first to get the latest news about trends, promotions, and
+            much more !
+          </p>
+          <div className='flex'>
+            <input
+              type='text'
+              placeholder='Email address'
+              className='p-4 w-3/4'
+            />
+            <button className=' w-1/4 bg-lama text-white'>JOIN</button>
+          </div>
+          <span className=' font-semibold'>Secure Payments</span>
+          <div className='flex justify-between gap-4 lg:flex-wrap'>
+            <Image src='/discover.png' alt='' width={40} height={40} />
+            <Image src='/skrill.png' alt='' width={40} height={40} />
+            <Image src='/paypal.png' alt='' width={40} height={40} />
+            <Image src='/mastercard.png' alt='' width={40} height={40} />
+            <Image src='/visa.png' alt='' width={40} height={40} />
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className='flex flex-col md:flex-row items-center justify-between gap-8 mt-16'>
+        <div className=''>&copy; copyright 2025 swimming Shop</div>
+        <div className='flex flex-col gap-8 md:flex-row'>
+          <div className=''>
+            <span className=' text-gray-500 mr-4'>Language</span>
+            <span className=' font-medium'>Morocco | English </span>
+          </div>
+          <div className=''>
+            <span className=' text-gray-500 mr-4'>Curency</span>
+            <span className=' font-medium'> MAD</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
